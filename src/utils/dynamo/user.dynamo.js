cfabd461ec64ec3cf7a2097ddb2f0b78046c8a84
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-06-20 16:43:48 +0530

    Create user.dynamo.js

---
+++ src/utils/dynamo/user.dynamo.js
@@ -0,0 +1,36 @@
+const { dynamoClient } = require('../../services/database');
+const TABLE_NAME = "Demo";
+
+const create = async (data) => {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: data
+    }
+    return await dynamoClient.put(params).promise();
+}
+
+const getUserByEmail = async (email) => {
+    var params = {
+        TableName: TABLE_NAME,
+        FilterExpression: "#email = :emailValue",
+        ExpressionAttributeNames: { "#email": "email" },
+        ExpressionAttributeValues: { ":emailValue": email }
+    };
+    return await dynamoClient.scan(params).promise();
+}
+
+const getAddressesBySK = async (SK) => {
+    var params = {
+        TableName: TABLE_NAME,
+        FilterExpression: "#SK = :skValue AND begins_with(#PK, :addRelation)",
+        ExpressionAttributeNames: { "#SK": "SK", "#PK": "PK" },
+        ExpressionAttributeValues: { ":skValue": SK, ":addRelation": "ADDRESS#" }
+    };
+    return await dynamoClient.scan(params).promise();
+}
+
+module.exports = {
+    create,
+    getUserByEmail,
+    getAddressesBySK
+}

75c0ea857ef50a292e5edf6dc5508dbb9bcbe3ba
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-06-20 18:05:03 +0530

    Update user.controlle.js

--- src/controllers/user.controlle.js
+++ src/controllers/user.controlle.js
@@ -1,7 +1,8 @@
 const { http, message, error_message } = require("../utils/constants/const");
 const { generateId } = require("../utils/helper");
 const { successResponse, respondWithError } = require("../utils/response.helper");
-const { create, getAddressesBySK } = require("../utils/dynamo/user.dynamo");
+const { create, getAddressesBySK, deleteItemBySK } = require("../utils/dynamo/user.dynamo");
+const { getItemById } = require("../utils/dynamo/pres.dynamo");
 
 exports.addAddress = async (req, res, next) => {
     try {
@@ -33,6 +34,24 @@ exports.getAddresses = async (req, res, next) => {
         const address = await getAddressesBySK(req.user.PK);
 
         return successResponse(res, http.StatusOK, message.ADDRESS_FETCH_SUCCEED, address.Items);
+    }
+    catch (error) {
+        return respondWithError(res, http.StatusInternalServerError, error_message.INTERNAL_ERROR, error);
+    }
+}
+
+exports.deleteAddress = async (req, res, next) => {
+    try {
+        if (!req.query.PK) {
+            return respondWithError(res, http.StatusBadRequest, error_message.INSUFFICIENT_DATA, undefined);
+        }
+        const address = await getItemById(req.query.PK, req.user.PK);
+        if(address.Item === undefined){
+            return respondWithError(res, http.StatusNotFound, error_message.ADDRESS_NOT_EXIST, undefined);
+        }
+        await deleteItemBySK(req.query.PK, req.user.PK);
+
+        return successResponse(res, http.StatusOK, message.ADDRESS_DETETED_SUCCESS, undefined);
     }
     catch (error) {
         return respondWithError(res, http.StatusInternalServerError, error_message.INTERNAL_ERROR, error);

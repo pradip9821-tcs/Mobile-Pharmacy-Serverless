9a5187d7f51bcb9eba6ad7582af9c336a106616a
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-06-20 18:05:05 +0530

    Update auth.routes.js

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -6,8 +6,6 @@ router.post('/signup', authController.signup);
 
 router.post('/login', authController.login);
 
-router.post('/refresh-token', authController.refreshToken)
-
-router.get('/test', authController.test)
+router.post('/refresh-token', authController.refreshToken);
 
 module.exports = router;
